48e6af799172d4a6fa39c35207a4571b7becb217
kennycastro007 <kennycastro007@gmail.com>
2021-04-26 23:05:21 -0400

    Implemented all requested features

--- InputReader.java
+++ InputReader.java
@@ -1,45 +1,78 @@
-import java.util.HashSet;
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.*;
 
 /**
- * InputReader reads typed text input from the standard text terminal. 
- * The text typed by a user is then chopped into words, and a set of words 
- * is provided.
+ * InputReader reads typed text input from the standard text terminal. The text
+ * typed by a user is then chopped into words, and a set of words is provided.
  * 
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
  */
-public class InputReader
-{
+public class InputReader {
     private Scanner reader;
+    private BufferedReader fileReader;
 
     /**
      * Create a new InputReader that reads text from the text terminal.
      */
-    public InputReader()
-    {
+    public InputReader() {
         reader = new Scanner(System.in);
+        Charset charset = Charset.forName("US-ASCII");
+        Path path = Paths.get("default.txt");
+        try {
+            fileReader = Files.newBufferedReader(path, charset);
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not find file 'default.txt'");
+            fileReader = null;
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            fileReader = null;
+        }
     }
 
     /**
-     * Read a line of text from standard input (the text terminal),
-     * and return it as a set of words.
+     * Read a line of text from standard input (the text terminal), and return it as
+     * a set of words.
      *
-     * @return  A set of Strings, where each String is one of the 
-     *          words typed by the user
+     * @return A set of Strings, where each String is one of the words typed by the
+     *         user
      */
-    public HashSet<String> getInput() 
-    {
-        System.out.print("> ");                // print prompt
-        String inputLine = reader.nextLine().trim().toLowerCase();
+    public HashMap<String, String> getInput() {
+        System.out.print("> "); // print prompt
 
-        String[] wordArray = inputLine.split(" ");  // split at spaces
+        HashMap<String, String> words = new HashMap<>();
 
-        // add words from array into hashset 
-        HashSet<String> words = new HashSet<>();
-        for(String word : wordArray) {
-            words.add(word);
+        try {
+            String firstLine = fileReader.readLine();
+            while (firstLine != null && !firstLine.equals("")) {
+                ArrayList<String> keys = new ArrayList<>();
+                String[] list = firstLine.trim().split(",");
+                for (String key : list) {
+                    keys.add(key.trim());
+                }
+                String value = "";
+                String line = fileReader.readLine();
+                while (line != null && !line.equals("")) {
+                    value += line + " ";
+
+                    line = fileReader.readLine();
+                }
+
+                for (String key : keys) {
+                    words.put(key, value);
+                }
+                firstLine = fileReader.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+
         return words;
     }
 }
--- SupportSystem.java
+++ SupportSystem.java
@@ -1,62 +1,77 @@
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
 
 /**
- * This class implements a technical support system.
- * It is the top level class in this project.
- * The support system communicates via text input/output
- * in the text terminal.
+ * This class implements a technical support system. It is the top level class
+ * in this project. The support system communicates via text input/output in the
+ * text terminal.
  * 
- * This class uses an object of class InputReader to read input
- * from the user, and an object of class Responder to generate responses.
- * It contains a loop that repeatedly reads input and generates
- * output until the users wants to leave.
+ * This class uses an object of class InputReader to read input from the user,
+ * and an object of class Responder to generate responses. It contains a loop
+ * that repeatedly reads input and generates output until the users wants to
+ * leave.
  * 
  * @author David J. Barnes and Michael Kölling.
  * @version 2016.02.29
  */
-public class SupportSystem
-{
+public class SupportSystem {
     private InputReader reader;
     private Responder responder;
-    
+
     /**
      * Creates a technical support system.
      */
-    public SupportSystem()
-    {
+    public SupportSystem() {
         reader = new InputReader();
         responder = new Responder();
     }
 
     /**
-     * Start the technical support system. This will print a welcome message and enter
-     * into a dialog with the user, until the user ends the dialog.
+     * Start the technical support system. This will print a welcome message and
+     * enter into a dialog with the user, until the user ends the dialog.
      */
-    public void start()
-    {
-        boolean finished = false;
+    public void start() {
+        // boolean finished = false;
 
         printWelcome();
 
-        while(!finished) {
-            HashSet<String> input = reader.getInput();
+        // while (!finished) {
+        HashMap<String, String> input = reader.getInput();
 
-            if(input.contains("bye")) {
-                finished = true;
-            }
-            else {
-                String response = responder.generateResponse(input);
-                System.out.println(response);
-            }
+        Set<String> keys = input.keySet();
+
+        for (String key : keys) {
+            System.out.println("Key[" + key + "]: " + input.get(key));
         }
+
+        ArrayList<String> testKeys = new ArrayList<>();
+        testKeys.add("crash");
+        testKeys.add("crashes");
+        if (keys.containsAll(testKeys)) {
+            System.out.println("All keys were found!");
+        } else {
+            System.out.println("Some keys were not found!");
+        }
+        if (input.get(testKeys.get(0)).equals(input.get(testKeys.get(1)))) {
+            System.out.println("All keys were given the correct value!");
+        } else {
+            System.out.println("Some keys were not given the correct value!");
+        }
+        // if (input.contains("bye")) {
+        // finished = true;
+        // } else {
+        // String response = responder.generateResponse(input);
+        // System.out.println(response);
+        // }
+        // }
         printGoodbye();
     }
 
     /**
      * Print a welcome message to the screen.
      */
-    private void printWelcome()
-    {
+    private void printWelcome() {
         System.out.println("Welcome to the DodgySoft Technical Support System.");
         System.out.println();
         System.out.println("Please tell us about your problem.");
@@ -67,8 +82,7 @@ public class SupportSystem
     /**
      * Print a good-bye message to the screen.
      */
-    private void printGoodbye()
-    {
+    private void printGoodbye() {
         System.out.println("Nice talking to you. Bye...");
     }
 }
